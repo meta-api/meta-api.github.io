8bb12923c1296e23937c79551b42812f593a0ab1
Ramlec <marcel@meta-api.io>
2021-01-28 12:19:17 +0100

    Deploy website - based on 7b07db1f4107e5a46ffbc1519b2a40a6ca6b392a

--- c4f5d8e4.823ba8d7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{84:function(t,e,n){"use strict";n.r(e);var a=n(0),r=n.n(a),c=(n(95),n(106),n(97),n(22)),i=(n(99),n(85),n(98));r.a.Fragment,r.a.Fragment,r.a.Fragment;e.default=function(){const t=Object(c.default)(),{siteConfig:e={}}=t;return r.a.createElement(i.Redirect,{to:"/docs/getting-started/quick-start"})}}}]);
